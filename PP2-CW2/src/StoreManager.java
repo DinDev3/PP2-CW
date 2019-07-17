3f95761ee866a4e8db3142c3076dc0a4a3be8eed
Dinuka <drpiyadigama@gmail.com>
2019-07-18 00:19:03 +0530

    Started working on PP2 CW2

--- PP2-CW2/src/StoreManager.java
+++ PP2-CW2/src/StoreManager.java
@@ -1,3 +1,5 @@
-public class StoreManager {
+interface StoreManager {
 //    public static final
+
+
 }
--- PP2-CW2/src/WestminsterMusicStoreManager.java
+++ PP2-CW2/src/WestminsterMusicStoreManager.java
@@ -4,4 +4,37 @@ public class WestminsterMusicStoreManager {
     static ArrayList <String> items= new ArrayList<>();
     static ArrayList <String> shoppingCart= new ArrayList<>();
 
+    public static void main(String[] args) {
+        MusicItem newItem = new MusicItem() {
+            @Override
+            String itemID() {
+                return null;
+            }
+
+            @Override
+            String title() {
+                return null;
+            }
+
+            @Override
+            String genre() {
+                return null;
+            }
+
+            @Override
+            Date releaseDate() {
+                return null;
+            }
+
+            @Override
+            String artist() {
+                return null;
+            }
+
+            @Override
+            Double price() {
+                return null;
+            }
+        }
+    }
 }
