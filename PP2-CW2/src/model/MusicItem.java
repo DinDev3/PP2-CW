419fb06b2bc1e719b7b7002442abfe07facca0b1
Dinuka <drpiyadigama@gmail.com>
2019-07-26 19:54:19 +0530

    Adding items looking good! XD

--- PP2-CW2/src/model/MusicItem.java
+++ PP2-CW2/src/model/MusicItem.java
@@ -2,34 +2,37 @@ package model;
 
 
 abstract class MusicItem {
-    private static int itemID;
+    private String itemID;
     private String title;
     private String genre;
     //private Date releaseDate;               //Lec 7 - slide 46!!!
     private String artist;
     private double price;
 
-    public MusicItem(String title, String genre, String artist, double price) {
+    private static int count;
+
+
+    public MusicItem(String itemID, String title, String genre, String artist, double price) {
+        this.itemID = itemID;
         this.title = title;
         this.genre = genre;
         this.artist = artist;
         this.price = price;
-
-        ++itemID;
+        count++;
     }
 
     @Override
     public String toString() {
         return "MusicItem{" +
-                "title='" + title + '\'' +
+                "itemID='" + itemID + '\'' +
+                ", title='" + title + '\'' +
                 ", genre='" + genre + '\'' +
                 ", artist='" + artist + '\'' +
                 ", price=" + price +
                 '}';
     }
 
-
-    public static int getItemID() {
-        return itemID;
+    public static int getCount() {
+        return count;
     }
 }
--- PP2-CW2/src/model/WestminsterMusicStoreManager.java
+++ PP2-CW2/src/model/WestminsterMusicStoreManager.java
@@ -6,9 +6,10 @@ import java.util.Scanner;
 public class WestminsterMusicStoreManager implements StoreManager {
     static Scanner sc = new Scanner(System.in);
 
-    static ArrayList<String> itemsInStore = new ArrayList<>();
-    static ArrayList<String> shoppingCart = new ArrayList<>();
+    static ArrayList<Object> itemsInStore = new ArrayList<>();
+    static ArrayList<Object> shoppingCart = new ArrayList<>();
 
+    static private String itemID;
     static private String title;
     static private String genre;
     static private double date;
@@ -19,7 +20,8 @@ public class WestminsterMusicStoreManager implements StoreManager {
     @Override
     public void addItem() {
         System.out.println("Choose item to be added:");
-        System.out.println("1)CD\n2)Vinyl\n>");        //validate!!!!!!
+        System.out.println("1)CD\n2)Vinyl");        //validate!!!!!!
+        System.out.print(">");
         int itemType = sc.nextInt();
         sc.nextLine();              //to consume the rest of the line
 
@@ -27,21 +29,27 @@ public class WestminsterMusicStoreManager implements StoreManager {
             addCommonInfo();        //used to get common information
 
             System.out.println("Enter Duration of song:");
+            System.out.print(">");
             duration = sc.nextDouble();
+            CD.cdInfo.put(title, duration);             //adding duration to hashMap
             sc.nextLine();              //to consume the rest of the line
 
-            CD newCD = new CD(title, genre, artist, price, duration);
+            CD newCD = new CD(itemID, title, genre, artist, price, duration);
+            itemsInStore.add(newCD);                            //adding CD object into itemsInStore arrayList
+
             System.out.println(newCD);
-            System.out.println(MusicItem.getItemID());              //to check, can be added to object as well!
+
+            System.out.println(MusicItem.getCount());           //to check whether count is right
+
+            //add object to itemsInStore arrayList!!!!!!!!!!!!!
 
         } else if (itemType == 2) {         //Vinyl item chosen
 
 
 
         }
-
-
     }
+
 
     @Override
     public void deleteItem() {
@@ -63,18 +71,32 @@ public class WestminsterMusicStoreManager implements StoreManager {
 
     }
 
+
+
     private static void addCommonInfo() {
-        System.out.println("\nEnter Title:");
+        System.out.println("\nEnter Item ID:");
+        System.out.print(">");
+        itemID = sc.nextLine();
+
+        System.out.println("Enter Title:");
+        System.out.print(">");
         title = sc.nextLine();
+
         System.out.println("Enter Genre:");
+        System.out.print(">");
         genre = sc.nextLine();
+
         System.out.println("Enter Release Date:");                      //Date class should be fixed as req!!!!
+        System.out.print(">");
         date = sc.nextDouble();
         sc.nextLine();              //to consume the rest of the line
 
         System.out.println("Enter Artist:");
+        System.out.print(">");
         artist = sc.nextLine();
+
         System.out.println("Enter Price:");
+        System.out.print(">");
         price = sc.nextDouble();
         sc.nextLine();              //to consume the rest of the line
     }
